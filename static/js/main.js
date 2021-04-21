4d8bc5177111323524c192c3dae0af9cbc6d2022
jakecorn <jakecorn135@gmail.com>
2021-04-21 23:01:59 +0800

    removed jarallax

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,10 @@ odoo.define('my_website.main', function (require) {
     "use strict";
 
 	$(function(){
-	    jarallax(document.querySelectorAll('.jarallax'));
-        jarallax(document.querySelectorAll('.jarallax-keep-img'), {
-            keepImg: true,
-        });
+//	    jarallax(document.querySelectorAll('.jarallax'));
+//        jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+//            keepImg: true,
+//        });
 	});
 
 });
