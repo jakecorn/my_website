cf102602fb3be411b8c343b75d34828b66216f78
jakecorn <jakecorn135@gmail.com>
2021-05-08 23:44:37 +0800

    homepage major updates, website builder snippets

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,17 @@
-# -*- coding: utf-8 -*-
 {
     'name': "my_website",
     'summary': "My Website",
-    'author': "My Company",
+    'author': "Jake Cornelia",
     'website': "http://www.yourcompany.com",
     'category': 'Uncategorized',
     'version': '0.1',
-    'depends': ['base', 'website', 'portal', 'website_form'],
+    'depends': ['base', 'website', 'portal', 'website_form', 'website_blog', 'crm', 'website_mass_mailing'],
     # always loaded
     'data': [
+        'views/backend/common.xml',
         'views/views.xml',
         'views/assets.xml',
+        'views/website_builder_snippet.xml',
         'views/homepage.xml',
         'views/templates.xml',
         'views/footer.xml',
@@ -20,3 +21,4 @@
         'data/fields.xml',
     ],
 }
+# -*- coding: utf-8 -*-
--- models/models.py
+++ models/models.py
@@ -6,3 +6,8 @@ from odoo import models, fields, api
 class Crmlead(models.Model):
     _inherit = "crm.lead"
     lead_type = fields.Char("Lead Type")
+
+class BlogPost(models.Model):
+    _inherit = "blog.post"
+    is_mini_guide = fields.Boolean(default=False)
+    teaser = fields.Text("Teaser Description")
