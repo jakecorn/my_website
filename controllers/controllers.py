551e05b1e2a08388b5bd45a5d807aed34b35f222
Jake Cornelia <jake@sciunitedllp.com>
2021-11-21 22:30:46 +0800

    changed the banner images, updated the banner text, added longpolling controller

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+from odoo.addons.bus.controllers.main import BusController
 
+class BusController(BusController):
+    # @http.route('/api/longpolling/send', type='json', auth='public')
+    @http.route('/api/longpolling/send', type="http", methods=['POST'], auth='public', csrf=False)
+    def api_send(self, channel=False, message=False, **post):
+        return self.send(channel, message)
 
-# class MyWebsite(http.Controller):
-#     @http.route('/my_website/my_website/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+    @http.route('/api/longpolling/poll', type="http", methods=['POST'], auth='public', csrf=False)
+    def api_poll(self, channels, last, options=None):
+        return self.poll(channels, last, options)
 
-#     @http.route('/my_website/my_website/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('my_website.listing', {
-#             'root': '/my_website/my_website',
-#             'objects': http.request.env['my_website.my_website'].search([]),
-#         })
+    @http.route('/my_website/my_website/objects/', auth='public')
+    def list(self, **kw):
+        return "asfsfsdfdf"
 
 #     @http.route('/my_website/my_website/objects/<model("my_website.my_website"):obj>/', auth='public')
 #     def object(self, obj, **kw):
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
+var ajax;
 odoo.define('my_website.main', function (require) {
     "use strict";
-
+    ajax = require('web.ajax');
 	$(function(){
 //	    jarallax(document.querySelectorAll('.jarallax'));
 //        jarallax(document.querySelectorAll('.jarallax-keep-img'), {
